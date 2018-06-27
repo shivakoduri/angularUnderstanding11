f7d8a2108e25a034cd67dd907f46873824de266a
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-06-27 23:20:52 +0530

    Unsubscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import 'rxjs/Rx';
 import {Observer} from "rxjs/Observer";
+import {Subscription} from "rxjs/Subscription";
 
 
 @Component({
@@ -9,17 +10,20 @@ import {Observer} from "rxjs/Observer";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit , OnDestroy{
+
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-     // const myNumbers = Observable.interval(1000);
-     // myNumbers.subscribe(
-     //   (number: number) => {
-     //     // console.log(number);
-     //   }
-     // );
+     const myNumbers = Observable.interval(1000);
+     this.numbersObsSubscription = myNumbers.subscribe(
+       (number: number) => {
+         console.log(number);
+       }
+     );
 
     const myObservable = Observable.create(
       (observer: Observer<any>) => {
@@ -33,7 +37,7 @@ export class HomeComponent implements OnInit {
           observer.error('this does not work');
         }, 5000);
       });
-      myObservable.subscribe(
+      this.customObsSubscription = myObservable.subscribe(
         (data: string) => { console.log(data)},
         (error: string) => {console.log(error)},
         () => {console.log('completed');}
@@ -41,4 +45,8 @@ export class HomeComponent implements OnInit {
 
   }
 
+  ngOnDestroy(){
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
+  }
 }
